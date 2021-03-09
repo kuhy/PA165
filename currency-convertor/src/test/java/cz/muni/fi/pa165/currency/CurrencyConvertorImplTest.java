66474703afaae3205d190f8d7b8996c0952c4031
Ondřej Kuhejda <ondrejkuhejda@gmail.com>
2021-03-09 22:51:11 +0100

    add testConvert

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,14 +1,55 @@
 package cz.muni.fi.pa165.currency;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 public class CurrencyConvertorImplTest {
+    @Mock
+    CurrencyConvertorImpl currencyConvertorMock;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(currencyConvertorMock.convert(Currency.getInstance("EUR"), Currency.getInstance("CZK"), new BigDecimal("1")))
+            .thenReturn(new BigDecimal("26.42"));
+        when(currencyConvertorMock.convert(Currency.getInstance("EUR"), Currency.getInstance("CZK"), new BigDecimal("0")))
+            .thenReturn(new BigDecimal("0"));
+        when(currencyConvertorMock.convert(Currency.getInstance("EUR"), Currency.getInstance("CZK"), new BigDecimal("2")))
+            .thenReturn(new BigDecimal("52.84"));
+        when(currencyConvertorMock.convert(Currency.getInstance("USD"), Currency.getInstance("CZK"), new BigDecimal("1")))
+            .thenReturn(new BigDecimal("22.26"));
+        when(currencyConvertorMock.convert(Currency.getInstance("USD"), Currency.getInstance("CZK"), new BigDecimal("2")))
+            .thenReturn(new BigDecimal("44.53"));
+        when(currencyConvertorMock.convert(Currency.getInstance("USD"), Currency.getInstance("CZK"), new BigDecimal("-1")))
+            .thenReturn(new BigDecimal("-22.26"));
+        when(currencyConvertorMock.convert(Currency.getInstance("USD"), Currency.getInstance("CZK"), new BigDecimal("1.5")))
+            .thenReturn(new BigDecimal("33.40"));
+    }
 
     @Test
     public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+        assertEquals(new BigDecimal("26.42"), currencyConvertorMock.convert(Currency.getInstance("EUR"),
+            Currency.getInstance("CZK"), new BigDecimal("1")));
+        assertEquals(new BigDecimal("0"), currencyConvertorMock.convert(Currency.getInstance("EUR"),
+            Currency.getInstance("CZK"), new BigDecimal("0")));
+        assertEquals(new BigDecimal("52.84"), currencyConvertorMock.convert(Currency.getInstance("EUR"),
+            Currency.getInstance("CZK"), new BigDecimal("2")));
+        assertEquals(new BigDecimal("22.26"), currencyConvertorMock.convert(Currency.getInstance("USD"),
+            Currency.getInstance("CZK"), new BigDecimal("1")));
+        assertEquals(new BigDecimal("44.53"), currencyConvertorMock.convert(Currency.getInstance("USD"),
+            Currency.getInstance("CZK"), new BigDecimal("2")));
+        assertEquals(new BigDecimal("-22.26"), currencyConvertorMock.convert(Currency.getInstance("USD"),
+            Currency.getInstance("CZK"), new BigDecimal("-1")));
+        assertEquals(new BigDecimal("33.40"), currencyConvertorMock.convert(Currency.getInstance("USD"),
+            Currency.getInstance("CZK"), new BigDecimal("1.5")));
     }
 
     @Test
